a5c44b3a75241ae6e37ed5d6f49a10abddc946ad
Mohad <36363227+TcpGuard@users.noreply.github.com>
2021-05-16 00:04:01 +0400

    Add files via upload

---
+++ capture.cpp
@@ -0,0 +1,44 @@
+// capture.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include<windows.h>
+#include <shellapi.h>
+#include <string>
+using namespace std;
+
+int main()
+{
+    
+    system("title TCPGuard.com");
+     cout << "TCPGuard.com Capture Packets" << endl;
+
+     wstring comm = L"";
+    
+     comm.append(L"/k cd C:\\Users\\Administrator\\Desktop\\");
+     comm.append(L" & C:\\Windows\\System32\\pktmon.exe start --etw -c");
+     comm.append(L" && exit ");
+
+	 HINSTANCE retVal = ShellExecuteW(NULL, L"open", L"cmd", comm.c_str(), NULL, SW_HIDE);
+
+     Sleep(10000);
+
+     comm = L"";
+     comm.append(L"/k C:\\Windows\\System32\\pktmon.exe stop");
+     comm.append(L" && exit ");
+
+     ShellExecuteW(NULL, L"open", L"cmd", comm.c_str(), NULL, SW_HIDE);
+
+     Sleep(10000);
+
+     comm = L"";
+     comm.append(L"/k cd C:\\Users\\Administrator\\Desktop\\");
+     comm.append(L" & C:\\Windows\\System32\\pktmon.exe pcapng PktMon.etl -o out.pcapng");
+     comm.append(L" && exit ");
+
+     ShellExecuteW(NULL, L"open", L"cmd", comm.c_str(), NULL, SW_HIDE);
+
+     system("pause");
+   
+
+}
